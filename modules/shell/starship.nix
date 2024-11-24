d7313072416f4f5bec8beddc5c042371611e7a3a
dasith <dasith@ebadfd.tech>
2024-11-25 01:19:14 +0530

    fix: added starship configs and updated zsh config

--- modules/shell/starship.nix
+++ modules/shell/starship.nix
@@ -1,16 +1,15 @@
+{ config, pkgs, ... }:
+
 {
+
+  home.packages = with pkgs; [
+    starship
+  ];
+
+  xdg.configFile."starship.toml".source = ./starship.toml;
+
   programs.starship = {
     enable = true;
-    # Configuration written to ~/.config/starship.toml
-    settings = {
-      # add_newline = false;
-
-      # character = {
-      #   success_symbol = "[➜](bold green)";
-      #   error_symbol = "[➜](bold red)";
-      # };
-
-      # package.disabled = true;
-    };
+    enableZshIntegration = true;
   };
 }
--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -11,12 +11,18 @@
       autosuggestions.enable = true;
       syntaxHighlighting.enable = true;
       enableCompletion = true;
+      shellAliases = { ll = "ls -al"; };
       histSize = 100000;
 
       ohMyZsh = {
         enable = true;
         plugins = [ "git" ];
       };
+
+      shellInit = ''
+        # starship
+        eval "$(starship init zsh)"
+      '';
     };
   };
 }
