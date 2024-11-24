8c91164df3c9e62b935e2d01daa5f1d86afa4cd3
dasith <dasith@ebadfd.tech>
2024-11-24 18:18:46 +0530

    feat: added hardware-configuration for vm

---
+++ hosts/vm/default.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot = {
+    loader = {
+      grub = {
+        enable = true;
+        device = "/dev/sda";
+        configurationLimit = 5;
+      };
+      timeout = 1;
+    };
+  };
+
+  bspwm.enable = true;
+
+  environment = {
+    systemPackages = with pkgs; [
+      hello # Hello World
+    ];
+  };
+
+  services = {
+    xserver = {
+      resolutions = [
+        { x = 1920; y = 1080; }
+        { x = 1600; y = 900; }
+        { x = 3840; y = 2160; }
+      ];
+    };
+  };
+}
+
