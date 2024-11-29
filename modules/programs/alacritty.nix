98da130d40504abc4c54da1b09615366df006efb
dasith <dasith@ebadfd.tech>
2024-11-29 11:14:12 +0530

    feat: added alacritty config for testing with new fonts

--- modules/programs/alacritty.nix
+++ modules/programs/alacritty.nix
@@ -5,15 +5,45 @@
     programs = {
       alacritty = {
         enable = true;
+
         settings = {
-          font = {
-            normal.family = "Fira Mono";
-            bold = { style = "Bold"; };
-            size = 11;
+          env = {
+            TERM = "xterm-256color";
+            WINIT_X11_SCALE_FACTOR = "1.0";
           };
-          offset = {
-            x = -1;
-            y = 0;
+
+          font = {
+            size = 12;
+
+            bold = {
+              family = "Fira Mono";
+              style = "Bold";
+            };
+
+            glyph_offset = {
+              x = 0;
+              y = 0;
+            };
+
+            italic = {
+              family = "Fira Mono";
+              style = "Italic";
+            };
+
+            normal = {
+              family = "Fira Mono";
+              style = "Regular";
+            };
+
+            offset = {
+              x = 0;
+              y = 0;
+            };
+          };
+
+          scrolling = {
+            history = 10000;
+            multiplier = 3;
           };
         };
       };
