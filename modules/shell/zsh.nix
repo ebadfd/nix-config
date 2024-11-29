7b8030978da802e0f1a57b2579c5f6bba69ee84b
dasith <dasith@ebadfd.tech>
2024-11-29 07:25:21 +0530

    feat: added zsh enable root

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -17,24 +17,45 @@ with lib;
       shell = pkgs.zsh;
     };
 
+    programs.zsh.enable = true;
+
     home-manager.users.${vars.user} = {
       programs = {
         zsh = {
           enable = true;
-          autosuggestions.enable = true;
-          syntaxHighlighting.enable = true;
-          enableCompletion = true;
-          shellAliases = {
-            ll = "ls -al";
-          };
-
-          histSize = 100000;
-
           oh-my-zsh = {
             enable = true;
             plugins = [ "git" ];
           };
-
+          plugins = [
+            {
+              name = "zsh-autosuggestions";
+              src = pkgs.fetchFromGitHub {
+                owner = "zsh-users";
+                repo = "zsh-autosuggestions";
+                rev = "v0.7.0";
+                sha256 = "1g3pij5qn2j7v7jjac2a63lxd97mcsgw6xq6k5p7835q9fjiid98";
+              };
+            }
+            {
+              name = "zsh-completions";
+              src = pkgs.fetchFromGitHub {
+                owner = "zsh-users";
+                repo = "zsh-completions";
+                rev = "0.34.0";
+                sha256 = "0jjgvzj3v31yibjmq50s80s3sqi4d91yin45pvn3fpnihcrinam9";
+              };
+            }
+            {
+              name = "zsh-syntax-highlighting";
+              src = pkgs.fetchFromGitHub {
+                owner = "zsh-users";
+                repo = "zsh-syntax-highlighting";
+                rev = "0.7.0";
+                sha256 = "0s1z3whzwli5452h2yzjzzj27pf1hd45g223yv0v6hgrip9f853r";
+              };
+            }
+          ];
           initExtra = ''
             export EDITOR=nvim
           '';
