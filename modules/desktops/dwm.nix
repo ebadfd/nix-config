41bf27a4bb7c3db1eda00fa2bb0ecf685c35d11d
dasith <dasith@ebadfd.tech>
2024-11-29 08:34:14 +0530

    chore: replace init login prompt

--- modules/desktops/dwm.nix
+++ modules/desktops/dwm.nix
@@ -14,10 +14,6 @@ with lib;
   config = mkIf (config.dwm.enable) {
     programs = {
       zsh.enable = true;
-      zsh.promptInit = ''
-        # Start graphical server on user's current tty if not already running.
-        [ "$(tty)" = "/dev/tty1" ] && ! pidof -s Xorg >/dev/null 2>&1 && exec startx "$XINITRC" &> /dev/null
-      '';
     };
 
     services = {
