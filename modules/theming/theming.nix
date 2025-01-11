cccd65b73e7127fb712b45048627a7d2a91c5bb7
dasith <dasith@ebadfd.tech>
2025-01-11 23:30:45 +0530

    fix: setup the icon theme for gtk

--- modules/theming/theming.nix
+++ modules/theming/theming.nix
@@ -8,6 +8,11 @@
   home-manager.users.${vars.user} = {
     gtk = lib.mkIf (config.gnome.enable == false) {
       enable = true;
+
+      iconTheme = {
+        name = "Papirus-Dark";
+        package = pkgs.papirus-icon-theme;
+      };
     };
 
     # qt = {
